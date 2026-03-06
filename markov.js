3bef3c6c543d40d53f362ee37c8d059f40b5d884
ash-pote <80757190+ash-pote@users.noreply.github.com>
2026-03-06 02:47:41 +0000

    Update markov.js

--- markov.js
+++ markov.js
@@ -1,6 +1,6 @@
 // https://mikebifulco.com/posts/javascript-filter-boolean
 
-let order = 1; // length of ngram
+let order = 3; // length of ngram
 let songLength = 1400;
 const synth = instruments[6]; // choose synth
 
